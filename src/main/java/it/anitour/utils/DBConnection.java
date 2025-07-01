105a594fb064ec18b215eab246e9f43c5085068a
Vi <89347463+Bing-88@users.noreply.github.com>
2025-07-01 20:58:18 +0200

    Update DBConnection.java
    codifica utf-8 (non ha funzionato, il carattere "à" nelle descrizioni dei tour viene visualizzato ancora male, poi vedo come aggiustare)

--- src/main/java/it/anitour/utils/DBConnection.java
+++ src/main/java/it/anitour/utils/DBConnection.java
@@ -3,7 +3,7 @@ package it.anitour.utils;
 import java.sql.*;
 
 public class DBConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/anitour";
+    private static final String URL = "jdbc:mysql://localhost:3306/anitour?useUnicode=true&characterEncoding=UTF-8&serverTimezone=UTC";
     private static final String USER = "root";
     private static final String PASSWORD = "root";
 
